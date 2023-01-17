1a28dbac8320d19a1acc44ba4491a1530282baf3
Zhirbarzan <zheer.ms@gmail.com>
2023-01-17 19:50:56 +0300

    Changed/Zhir

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,6 +28,7 @@ namespace BackupSystemTool
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            Debug.WriteLine("aa");
 
         }
     }
