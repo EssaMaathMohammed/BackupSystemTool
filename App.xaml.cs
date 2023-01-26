d3404e32edbc112c178a34882d770b34568d27a7
EssaMaath <eisa.2001072383@spu.edu.iq>
2023-01-26 14:46:46 +0300

    Database Update: Added: - sqlite-net-pcl package - ConnectionItem Class - DatabaseTester Class - Connections Page (includes connections, backup scheduling)
    Additions to:
    - DatabaseTester Class
     A- Insert To connection database function
    
    - App.xaml.cs
     A- public Database Path to be used in the rest of the application
     B- Create Directory in AppData for the ConnectionsDB.db

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +14,20 @@ namespace BackupSystemTool
     /// </summary>
     public partial class App : Application
     {
+        // Creates a path to the database directory location + Database Name
+        static string ApplicationFileName = "BackupSystemTool";
+        static string databaseName = "ConnectionsDB.db";
+        static string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        static string databaseFolderPath = Path.Combine(folderPath, ApplicationFileName);
+        public static string databasePath = Path.Combine(databaseFolderPath, databaseName);
+        public App() {
+            // checks if directory exists, if not creates it.
+            if (!Directory.Exists(databaseFolderPath))
+            {
+                Directory.CreateDirectory(databaseFolderPath);
+            }
+        }
+
     }
+    
 }
