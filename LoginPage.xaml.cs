2d8592e4e0cb2633d923e0e3fd95e2fe441ca022
EssaMaath <eisa.2001072383@spu.edu.iq>
2023-02-05 17:17:18 +0300

    Additions: - System.Management Package - PIN validation method - getCPUID() method - getMacAddress() method - generateSalt method - hashKey() method - setKey() method - getKey() method - evaluateKey() method - generate a key (App.xaml)
    Details:
    - PIN Validation:
     * notify the user in case the PIN is too short, has the wrong format, or empty
    - getCPUID, getMacAddress:
     * bring the CPUID and MacAddress of the local machine
    - generateSalt:
     * generates salt using RandomNumberGenerator to be mixed with the CPUID, MacAddress
    - hashKey:
     * uses SHA256 to hash the generated key (CPUID, MAC, SALT)
    - setKey:
     * stores the generated key in the registry
    - getKey:
     * gets the stored key from the registry
    - evaluateKey:
     * checks whether a key exists in the registry
            if the key exists the getKey method will be called
       if the key doenst exist a new key will be generated
    
    -- Private --
    Key generation is done using the following:
     * CPUID
     * MAC_ADDRESS
     * SALT
     * SHA256 (16 fixed length)
    using the following combination a key will be created and stored
    in the registry under the KeyName: BackupSystemTool and SubKeyName of EncryptionKey
    -- Private --

--- LoginPage.xaml.cs
+++ LoginPage.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Gu.Wpf.Adorners;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -28,11 +30,38 @@ namespace BackupSystemTool
 
         private void loginButton_Click(object sender, RoutedEventArgs e)
         {
-            ConnectionsPage connectionsPage = new ConnectionsPage();
-            connectionsPage.Show();
-            this.Close();
+            if (ValidatePin())
+            {
+                ConnectionsPage connectionsPage = new ConnectionsPage();
+                connectionsPage.Show();
+                this.Close();
+            }
         }
 
-        
+        private bool ValidatePin()
+        {
+            if (string.IsNullOrEmpty(PINTextBox.Password))
+            {
+                MessageBox.Show("Please enter PIN", "Validation Error", MessageBoxButton.OK, MessageBoxImage.Information);
+                return false;
+            }
+
+            if (!Regex.IsMatch(PINTextBox.Password, @"^[a-zA-Z0-9!@#\$%\^&\*()_+-=\{\}\[\];':\"",\.<>\/\?]+$"))
+            {
+                MessageBox.Show("PIN can only contain letters, numbers, and special characters", "Validation Error", MessageBoxButton.OK, MessageBoxImage.Information);
+                return false;
+            }
+
+            if (PINTextBox.Password.Length < 6 || PINTextBox.Password.Length > 12)
+            {
+                MessageBox.Show("PIN must be between 6 and 12 characters long", "Validation Error", MessageBoxButton.OK, MessageBoxImage.Information);
+                return false;
+            }
+
+            return true;
+        }
+
+
+
     }
 }
